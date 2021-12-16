d1058bcebeac1914844d58ff80176015be712142
Terra Taylor <terra.taylor@gmail.com>
2021-12-16 07:45:56 -0600

    2 solutions

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class MovieService {
 
   constructor(private http:HttpClient) { }
-
+  public movies:Array<any> = [];
   public searchMovies(searchText:string){
     return new Promise((resolve,reject)=>{
       this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=5c06fed2cdf4dfcdab132d9e67c1c2e7&language=en-US&query=${searchText}&page=1&include_adult=false`).subscribe(
